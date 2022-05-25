c03c7e05d665fbcecdacb17c2aac6c26410f7fe8
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-05-25 11:16:16 -0500

    can't see login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,16 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
+    
+    <Routes>
+        <Route exact path="/" element={<Login />}>
         </Route>
-      </Switch>
-    </Router>
+        </Routes>
+    
   </div>
   );
 }
