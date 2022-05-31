7bb1cb94ec2d023ff1d1d673cc1081ac3036f8b3
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-05-31 11:10:49 -0500

    header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import { Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <Header/>
     <Routes>
         <Route exact path="/" element={<Login />}>
         </Route>
